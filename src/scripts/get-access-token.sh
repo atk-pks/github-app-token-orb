6ca1691eaf86388db0873c5d051f443d53264c8c
RossyWhite <daikishiroi@gmail.com>
2023-01-15 18:02:52 +0900

    write tmp file

--- src/scripts/get-access-token.sh
+++ src/scripts/get-access-token.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 # reference: https://docs.github.com/en/developers/apps/building-github-apps/authenticating-with-github-apps#authenticating-as-a-github-app
-
-set -euo pipefail
-
 b64enc() { openssl enc -base64 -A | tr '+/' '-_' | tr -d '='; }
 
 app_id=${!APP_ID_ENV:-}
@@ -15,10 +12,11 @@ iat=$(($(date +%s) - 60))
 exp="$((iat + duration_seconds))"
 
 # create the JWT
+echo "$app_private_key" > app_private_key.pem
 signed_content="$(echo -n '{"alg":"RS256","typ":"JWT"}' | b64enc).$(echo -n "{\"iat\":${iat},\"exp\":${exp},\"iss\":${app_id}}" | b64enc)"
-sig=$(echo -n "$signed_content" | openssl dgst -binary -sha256 -sign <(printf '%s\n' "$app_private_key") | b64enc)
+sig=$(echo -n "$signed_content" | openssl dgst -binary -sha256 -sign app_private_key.pem | b64enc)
 jwt=$(printf '%s.%s\n' "${signed_content}" "${sig}")
-
+rm app_private_key.pem
 
 # get the access token
 org="${CIRCLE_PROJECT_USERNAME}"
