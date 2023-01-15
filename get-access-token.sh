a361366770a2ad245a407adbc18716e7dd07257c
RossyWhite <daikishiroi@gmail.com>
2023-01-15 13:29:17 +0900

    Get access token

--- get-access-token.sh
+++ get-access-token.sh
@@ -8,34 +8,31 @@ b64enc() { openssl enc -base64 -A | tr '+/' '-_' | tr -d '='; }
 
 app_id="${APP_ID}"
 app_private_key="${APP_PRIVATE_KEY}"
-session_time="${SESSION_TIME:-300}"
+duration_seconds="${DURATION_SECONDS:-300}"
 
 # issued at time, 60 seconds in the past to allow for clock drift
 iat=$(($(date +%s) - 60))
-exp="$((iat + session_time))"
+exp="$((iat + duration_seconds))"
 
-signed_content="$( echo -n '{"alg":"RS256","typ":"JWT"}' | b64enc).$(echo -n "{\"iat\":${iat},\"exp\":${exp},\"iss\":${app_id}}" | b64enc)"
+# create the JWT
+signed_content="$(echo -n '{"alg":"RS256","typ":"JWT"}' | b64enc).$(echo -n "{\"iat\":${iat},\"exp\":${exp},\"iss\":${app_id}}" | b64enc)"
 sig=$(echo -n "$signed_content" | openssl dgst -binary -sha256 -sign <(printf '%s\n' "$app_private_key") | b64enc)
 jwt=$(printf '%s.%s\n' "${signed_content}" "${sig}")
 
-echo $jwt
+installation_id=$(curl -s \
+	-H "Accept: application/vnd.github+json" \
+	-H "Authorization: Bearer ${jwt}" \
+	-H "X-GitHub-Api-Version: 2022-11-28" \
+	https://api.github.com/repos/$ORG/$REPO/installation | jq -r '.id')
 
-#curl -s \
-#    -H "Authorization: Bearer ${jwt}" \
-#    -H "Accept: application/vnd.github.machine-man-preview+json" \
-#    "https://api.github.com/app"
+access_token=$(curl -s -X POST \
+	-H "Authorization: Bearer $jwt" \
+	-H "Accept: application/vnd.github+json" \
+	-H "X-GitHub-Api-Version: 2022-11-28" \
+	https://api.github.com/app/installations/"${installation_id}"/access_tokens | jq -r '.token')
 
-#curl -i -X GET \
-#-H "Authorization: Bearer $jwt" \
-#-H "Accept: application/vnd.github+json" \
-#https://api.github.com/app/installations
-
-#curl -i -X POST \
-#-H "Authorization: Bearer $jwt" \
-#-H "Accept: application/vnd.github+json" \
-#https://api.github.com/app/installations/32480052/access_tokens
-
-#curl -i \
-#-H "Authorization: Bearer ghs_WpLYMc4YiNMm14fKibGDb94DBMKYno08zv7h" \
-#-H "Accept: application/vnd.github+json" \
-#https://api.github.com/installation/repositories
+curl -i \
+	-H "Authorization: Bearer $access_token" \
+	-H "Accept: application/vnd.github+json" \
+	-H "X-GitHub-Api-Version: 2022-11-28" \
+	https://api.github.com/orgs/$ORG/repos
